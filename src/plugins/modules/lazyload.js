c9e3ff11fc3372be2b17f5c1d180ad518e98397b
guogangchuan <897674352@qq.com>
2019-06-12 14:04:07 +0800

    图片目录变更

--- src/plugins/modules/lazyload.js
+++ src/plugins/modules/lazyload.js
@@ -7,5 +7,5 @@ Vue.use(VueLazyload, {
   preLoad: 1.3, // 预加载高度，为屏幕的倍数
   attempt: 2, // 尝试加载次数
   // loading: "", // loading时显示的图片
-  error: require("@/assets/imgs/global/pic/img_load_fail.png") // 图片加载失败时显示的图片
+  error: require("@/assets/imgs/global/pic/pic_load_fail.png") // 图片加载失败时显示的图片
 });
